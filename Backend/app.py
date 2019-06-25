afafa16463606f9f0d05ae1436324ab7939b8825
Matías Hermosilla <matiasahermosilla@gmail.com>
2019-06-24 21:53:04 -0400

    Quitando modo debug y cambiando comando para inicio de backend

--- Backend/app.py
+++ Backend/app.py
@@ -20,4 +20,4 @@ app.register_blueprint(optical.mod)
 app.register_blueprint(procesado.mod)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True)    
+    app.run(host="0.0.0.0")    
